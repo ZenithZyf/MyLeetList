929dc4bd9cd264c161c2fcf085b04dd64fe6910a
ZenithZyf <zenithzyf@gmail.com>
2020-08-27 00:57:53 -0500

    Create LC977-SquaresOfASortedArray.cpp

---
+++ TwoPointer/LC977-SquaresOfASortedArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> ans(A.size());
+        auto e = prev(end(A));
+        auto s = begin(A);
+        auto it = end(ans);
+        while (--it >= begin(ans)) {
+          if (abs(*e) > abs(*s))
+            *it = pow(*e--, 2);        
+          else
+            *it = pow(*s++, 2);
+        }
+        return ans;
+    }
+};
