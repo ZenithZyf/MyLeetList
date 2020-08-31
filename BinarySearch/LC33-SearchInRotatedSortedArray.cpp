474d87e39094e49a41ca2697694a6be77d78a530
ZenithZyf <zenithzyf@gmail.com>
2020-08-30 21:40:58 -0500

    Create LC33-SearchInRotatedSortedArray.cpp

---
+++ BinarySearch/LC33-SearchInRotatedSortedArray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()) return false;
+        
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            
+            if (nums[m] >= nums[l])
+                if (nums[m] > target && nums[l] <= target)
+                    r = m - 1;
+                else
+                    l = m + 1;
+            else
+                if (nums[m] < target && nums[r] >= target)
+                    l = m + 1;
+                else
+                    r = m - 1;
+        }
+        
+        return -1;
+    }
+};
