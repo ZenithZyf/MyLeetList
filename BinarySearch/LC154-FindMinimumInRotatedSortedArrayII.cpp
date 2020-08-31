70b3462eaf3c0774266ad5a88df680ed8f8cc6f8
ZenithZyf <zenithzyf@gmail.com>
2020-08-31 11:39:03 -0500

    Create LC154-FindMinimumInRotatedSortedArrayII.cpp

---
+++ BinarySearch/LC154-FindMinimumInRotatedSortedArrayII.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        return findMin(nums, 0, nums.size()-1);
+    }
+private:
+    int findMin(vector<int>& nums, int l, int r) {
+        if (l+1 >= r) return min(nums[l], nums[r]);
+        if (nums[l] < nums[r]) return nums[l];
+        int m = l + (r-l)/2;
+        return min(findMin(nums, l, m-1),
+                   findMin(nums, m, r));
+    }
+};
