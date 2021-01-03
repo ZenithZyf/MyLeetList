32239fc099806e55238199edf0343774230428fa
ZenithZyf <zenithzyf@gmail.com>
2021-01-02 20:05:55 -0600

    Create LC66-PlusOne.cpp

---
+++ Array/LC66-PlusOne.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            }
+            else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        vector<int> new_digits(n + 1, 0);
+        new_digits[0] = 1;
+        return new_digits;
+    }
+};
