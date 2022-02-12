109509758430b2e6f6f63831e8123fdd06e8ce40
ZenithZyf <zenithzyf@gmail.com>
2022-02-12 14:41:02 -0600

    Create LC34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp

---
+++ Leet2022/BinarySearch/LC34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {leftBound(nums, target), rightBound(nums, target)};
+    }
+private:
+    int leftBound(const vector<int>& nums, const int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid - 1;
+            }
+        }
+        if (left >= nums.size() || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+    
+    int rightBound(const vector<int>& nums, const int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                left = mid + 1;
+            }
+        }
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
+    }
+};
