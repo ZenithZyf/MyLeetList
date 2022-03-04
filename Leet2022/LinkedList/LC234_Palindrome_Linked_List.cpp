7b87595199194c77aa81034527873510f2b99bab
ZenithZyf <zenithzyf@gmail.com>
2022-03-04 15:36:00 -0800

    Create LC234_Palindrome_Linked_List.cpp

---
+++ Leet2022/LinkedList/LC234_Palindrome_Linked_List.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if (fast) {
+            slow = slow->next;
+        }
+        ListNode* left = head;
+        ListNode* right = reverse(slow);
+        while (right) {
+            if (left->val != right->val) {
+                return false;
+            }
+            left = left->next;
+            right = right->next;
+        }
+        return true;
+    }
+private:
+    ListNode* reverse(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+};
