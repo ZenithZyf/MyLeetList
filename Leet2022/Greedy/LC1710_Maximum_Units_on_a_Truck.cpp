c3b7685000fed028741b9581a9a578b28799ed33
ZenithZyf <zenithzyf@gmail.com>
2022-03-12 21:55:06 -0800

    Update LC1710_Maximum_Units_on_a_Truck.cpp

--- Leet2022/Greedy/LC1710_Maximum_Units_on_a_Truck.cpp
+++ Leet2022/Greedy/LC1710_Maximum_Units_on_a_Truck.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        std::sort(begin(boxTypes), end(boxTypes), [&](vector<int> a, vector<int> b){
+        std::sort(begin(boxTypes), end(boxTypes), [](const vector<int>& a, const vector<int>& b){
            return a[1] > b[1]; 
         });
         int ans = 0;
-        for (int i = 0; i < boxTypes.size(); ++i) {
-            if (truckSize == 0) {
+        for (const auto& b : boxTypes) {
+            ans += b[1] * min(b[0], truckSize);
+            if ((truckSize -= b[0]) <= 0) {
                 break;
-            }
-            if (boxTypes[i][0] <= truckSize) {
-                ans += boxTypes[i][0] * boxTypes[i][1];
-                truckSize -= boxTypes[i][0];
-            } else {
-                ans += truckSize * boxTypes[i][1];
-                truckSize = 0;
             }
         }
         return ans;
