85a81a83cd273293d9efbe8a8483211bacd142b3
ZenithZyf <zenithzyf@gmail.com>
2022-04-02 20:31:58 -0700

    Create LC362_Design_Hit_Counter.cpp

---
+++ Leet2022/Design/LC362_Design_Hit_Counter.cpp
@@ -0,0 +1,37 @@
+class HitCounter {
+public:
+    HitCounter() {
+        times.resize(300);
+        hits.resize(300);
+    }
+    
+    void hit(int timestamp) {
+        int idx = timestamp % 300;
+        if (times[idx] != timestamp) {
+            times[idx] = timestamp;
+            hits[idx] = 1;
+        } else {
+            ++hits[idx];
+        }
+    }
+    
+    int getHits(int timestamp) {
+        int res = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - times[i] < 300) {
+                res += hits[i];
+            }
+        }
+        return res;
+    }
+private:
+    vector<int> times;
+    vector<int> hits;
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
