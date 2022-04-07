d3cf1d11168e747d3cbfae2be1c3cee1806dc38c
ZenithZyf <zenithzyf@gmail.com>
2022-04-06 21:30:00 -0700

    Create LC523_Continuous_Subarray_Sum.cpp

---
+++ Leet2022/PrefixSum/LC523_Continuous_Subarray_Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        m[0] = -1;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            if (k != 0) {
+                sum %= k;
+            }
+            if (m.count(sum)) {
+                if (i - m.at(sum) > 1) {
+                    return true;
+                }
+            } else {
+                m[sum] = i;
+            }
+        }
+        return false;
+    }
+};
