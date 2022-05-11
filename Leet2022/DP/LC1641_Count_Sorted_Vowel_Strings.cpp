79d77e2e755bd62e4a6e5b1ccc7f74f374cc572b
ZenithZyf <zenithzyf@gmail.com>
2022-05-11 10:00:08 -0700

    Create LC1641_Count_Sorted_Vowel_Strings.cpp

---
+++ Leet2022/DP/LC1641_Count_Sorted_Vowel_Strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        // dp[i][j] = # of strings of length i ends with j
+        vector<vector<int>> dp(n + 1, vector<int>(5, 1));
+        for (int i = 2; i <= n; ++i) {
+            int s = 0;
+            for (int j = 0; j < 5; ++j) {
+                s += dp[i - 1][j];
+                dp[i][j] = s;
+            }
+        }
+        return std::accumulate(begin(dp[n]), end(dp[n]), 0);
+    }
+};
